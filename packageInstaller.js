12815c9979d12ec948869efa87be26ad98fd54de
mpalamar <mpalamar92@gmail.com>
2017-07-01 17:54:39 -0700

    Added comments and readme

--- packageInstaller.js
+++ packageInstaller.js
@@ -3,7 +3,8 @@
 var packageInstaller = {
 	orderedPackages: [],
 	errorDetected: false,
-	// This function cycle's through a packages dependency reference
+	// This function cycle's through a package's dependency references to check if a cycle occurs. Sets a flag 
+	// if an error is detected.
 	checkForCycle: (dep) => {
 	    if (dep) {
 	        let depList = [dep];
@@ -36,6 +37,7 @@ var packageInstaller = {
 			return 'Error: Cycle Detected';
 		}
 	},
+	// Receives an array of 'Package : Dependency' items. Each item is added and checked to ensure that it has a name, and no cycles
 	install: function(packages) {
 		if (packages.length) {
 			for (let pkg of packages) {
@@ -50,6 +52,8 @@ var packageInstaller = {
 			return packageInstaller.printDependencies();			
 		}
 	},
+	//Receives a single package and checks if it or it's dependency already exist. The algorithm makes sure that the package's dependency
+	//is added to the data structure prior to the actual package. 
 	addDependency: (pkg) => {
         let pkgExists = packageInstaller.orderedPackages.find((el) => el.name === pkg.name);
         if (!pkgExists) {
@@ -59,19 +63,23 @@ var packageInstaller = {
                     pkg.depRef = depExists;
                     packageInstaller.checkForCycle(depExists);
                 } else {
+                	//No need to reorganize the array, since this dependency is added prior to the package which requires it
                     pkg.depRef = {name: pkg.depName, depName: null, depRef: null};
                     packageInstaller.orderedPackages.push(pkg.depRef);
                 }
             }
             packageInstaller.orderedPackages.push(pkg);   
         } else {
+        	//No need to add the package since it already exists. 
             if (pkg.depName.length) {
             	let newDep = {name: pkg.depName, depName: null, depRef: null};
                 let depExists = packageInstaller.orderedPackages.find((el) => el.name === newDep.name);
                 if (depExists) {
+                	//must check for cycles if the dependency already exsists.
                     pkgExists.depRef = depExists;
                     packageInstaller.checkForCycle(depExists);
                 } else {
+                	//array order must be reorganized to insert the dependency before the package that requires it.
                     pkgExists.depRef = newDep;
                     pkgExists.depName = newDep.name;
                     let index = packageInstaller.orderedPackages.findIndex((el) => el.depName === newDep.name)
